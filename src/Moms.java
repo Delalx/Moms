36916a892d4f7f53892289c6f5907fe1642ad330
Delalx <Delal-kesen@outlook.com>
2020-09-16 14:16:27 +0200

    Initial commit

---
+++ src/Moms.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Moms {
+
+    public static void main(String[] args) {
+        plusMoms();
+        plusTyskMoms();
+
+    }
+
+    public static void plusMoms() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Indtast købsbeløbet:");
+        double købsbeløbet = in.nextDouble();
+
+        double moms = købsbeløbet * 0.25; //Dansk moms er 25%
+        System.out.println("Momsen er i DKK " + (int) (moms * 100) / 100.0);
+    }
+
+    public static void plusTyskMoms() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Indtast købsbeløbet:");
+        double købsbeløbet = in.nextDouble();
+
+        double Tyskmoms = købsbeløbet * 0.16; //Tysk moms er 16%
+        System.out.println("Momsen er i euro " + (int) (Tyskmoms * 100) / 100.0);
+
+    }
+}
