a13ebd8483dff879fb3f443afa733d7350a98af0
Delalx <Delal-kesen@outlook.com>
2020-09-16 14:19:32 +0200

    Ændret navn på en metode

--- src/Moms.java
+++ src/Moms.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Moms {
 
     public static void main(String[] args) {
-        plusMoms();
+        plusDanskMoms();
         plusTyskMoms();
 
     }
 
-    public static void plusMoms() {
+    public static void plusDanskMoms() {
         Scanner in = new Scanner(System.in);
         System.out.println("Indtast købsbeløbet:");
         double købsbeløbet = in.nextDouble();
