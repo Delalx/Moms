12ff39b7d6959d0ba91fd1010cae6a3be2b040ef
Delalx <Delal-kesen@outlook.com>
2020-09-16 14:32:47 +0200

    Tilføjet tysk moms

--- src/Moms.java
+++ src/Moms.java
@@ -4,6 +4,7 @@ public class Moms {
 
     public static void main(String[] args) {
         plusMoms();
+        plusTyskMoms();
 
     }
 
@@ -15,5 +16,14 @@ public class Moms {
         double moms = købsbeløbet * 0.25; //Dansk moms er 25%
         System.out.println("Momsen er " + (int) (moms * 100) / 100.0);
     }
+    public static void plusTyskMoms() {
+        Scanner in = new Scanner(System.in);
 
+        System.out.println("Indtast købsbeløbet:");
+        double købsbeløbet = in.nextDouble();
+
+        double Tyskmoms = købsbeløbet * 0.16; //Tysk moms er 16%
+        System.out.println("Momsen er " + (int) (Tyskmoms * 100) / 100.0);
+
+    }
 }
